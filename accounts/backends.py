9d538f29967b81b505604aa81844486fe279f662
mNiloy695 <mniloy695@gmail.com>
2025-06-19 17:14:50 +0600

    phoneModelUpdated

--- accounts/backends.py
+++ accounts/backends.py
@@ -2,22 +2,18 @@ from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 class PhoneBackend(ModelBackend):
-
-
-    # set phone number as a login parameter
     def authenticate(self, request, phone=None, password=None, **kwargs):
         UserModel = get_user_model()
-        u=UserModel.objects.get(phone=phone)
-        print("The user is : ",UserModel.objects.get(phone=phone))
-        user = None
+
         if phone is None:
             phone = kwargs.get('phone')
+
         if phone and password:
             try:
                 user = UserModel.objects.get(phone=phone)
-                
+                if user.check_password(password):
+                    return user
             except UserModel.DoesNotExist:
                 return None
-            if user.check_password(password):
-                return user
+
         return None
--- town_market/settings.py
+++ town_market/settings.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'town_market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # or your actual database name
+        'USER': 'postgres.uimoziaetqawnhkxdpva',  # Replace this with your actual username
+        'PASSWORD': env("PASSWORD_DATABASE"),  # Ensure this is set in .env
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
